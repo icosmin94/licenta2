046141d407fa56553246d546bf219ae25b725814
Cosmin Iacob <i.cosmin94@yahoo.com>
2017-05-31 11:30:45 +0300

    stopwords & contractions support modularazation

--- test.py
+++ test.py
@@ -1,24 +1,8 @@
 import configparser
-import re
 from pymongo import MongoClient
 from tweet import *
+import json
 
-
-def create_tweet(tweet_line):
-    parts = re.split(r'[,"\[\]]+', tweet_line)
-    print(parts)
-    author = parts[1]
-    age = parts[2]
-    gender = parts[3]
-    latitude = parts[4]
-    longitude = parts[5]
-    date_time = parts[parts.__len__()-1]
-    date_time_parts = re.split('[TZ\n]+', date_time)
-    raw_text = re.sub(r'\\', '', tweet_line[tweet_line.index(parts[6]): tweet_line.index(date_time) - 1])
-
-    #print(re.sub(r'[!?.\\<>\[\]()/*:+~=;^\']+', '', raw_text).lower())
-    tweet = Tweet(author, age, gender, latitude, longitude, date_time_parts[0], date_time_parts[1], raw_text, 0, {})
-    print(tweet.__dict__)
 
 if __name__ == "__main__":
 
@@ -26,14 +10,26 @@ if __name__ == "__main__":
     config.read('./config/config.ini')
 
     tweetsFile = config['default']['tweets_file']
-    print(tweetsFile)
 
     client = MongoClient(config['database']['host'], int(config['database']['port']))
     db = client[config['database']['db']]
 
-    i = 0
+    # read stop words
+    stop_words = []
+    with open('./words/stop_words.txt') as sp:
+        for line in sp:
+            stop_words.extend([line.strip(" \n")])
+
+    # read contractions file
+    with open('./words/contractions.json') as contractions_file:
+        contractions = json.load(contractions_file)
+
+    line_number = 0
     with open(tweetsFile) as fp:
         for line in fp:
-            i += 1
-            if i > 1:
-                create_tweet(line)
+            line_number += 1
+            if line_number > 1:
+                create_tweet(line, contractions, stop_words)
+
+
+
--- tweet.py
+++ tweet.py
@@ -1,3 +1,9 @@
+import re
+from nltk.corpus import wordnet
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+
 class Tweet:
 
     def __init__(self, author, age, gender, latitude,
@@ -13,5 +19,67 @@ class Tweet:
         self.raw_text = raw_text
         self.words_count = words_count
         self.words_map = words_map
-        self.tfIdfMap = {}
+
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+
+def lemmatize(tweet, contractions, stop_words):
+    contractionless_words = []
+    processed_words = []
+    words_map = {}
+
+    words = re.split(r' ', tweet.raw_text)
+
+    for word in words:
+        if word.lower() in contractions:
+            contractionless_words.extend(contractions[word.lower()])
+        else:
+            contractionless_words.extend([word.lower()])
+
+    for word in contractionless_words:
+        if word.endswith("'s"):
+            the_word = re.sub("'s", '', word)
+        else:
+            the_word = word
+        if the_word not in stop_words and the_word.__len__() > 1:
+            processed_words.extend([the_word])
+
+    tweet.words_count = processed_words.__len__()
+    tagged = nltk.pos_tag(processed_words)
+    word_net_lemmatizer = WordNetLemmatizer()
+
+    for i in range(processed_words.__len__()):
+        lemma = word_net_lemmatizer.lemmatize(tagged[i][0], get_wordnet_pos(tagged[i][1]))
+        if lemma not in words_map:
+            words_map[lemma] = 0
+        words_map[lemma] = words_map[lemma] + 1
+
+    tweet.words_map = words_map
+
+
+def create_tweet(tweet_line, contractions, stop_words):
+    parts = re.split(r'[,"\[\]]+', tweet_line)
+    author = parts[1]
+    age = parts[2]
+    gender = parts[3]
+    latitude = parts[4]
+    longitude = parts[5]
+    date_time = parts[parts.__len__()-1]
+    date_time_parts = re.split('[TZ\n]+', date_time)
+    raw_text = re.sub(r"[!?.\\<>\[\]()/,*:+~=\-;^\"]+", '', tweet_line[tweet_line.index(parts[6]): tweet_line.index(date_time) - 1])
+
+    tweet = Tweet(author, age, gender, latitude, longitude, date_time_parts[0], date_time_parts[1], raw_text, 0, {})
+    lemmatize(tweet, contractions, stop_words)
+    return tweet
 
