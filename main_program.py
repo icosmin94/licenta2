899952c7b887e9f9302b1f8a1e6f08fbff710cd9
Cosmin Iacob <i.cosmin94@yahoo.com>
2017-06-07 02:02:05 +0300

    process topics extract topic centroids

--- main_program.py
+++ main_program.py
@@ -1,161 +1,43 @@
 import configparser
-from concurrent.futures import ProcessPoolExecutor
-from pprint import pprint
-import re
-import numpy as np
-import time
-import datetime
 
-import pymongo
-from scipy.sparse import csr_matrix
-import math
 from pymongo import MongoClient
+
+from create_topics import create_and_store_topics
 from load_tweets import load_tweets
 from topics import Topic
-from tweet import Tweet
-from sklearn.decomposition import NMF
-
-
-def compute_nmf(config, start_datetime, stop_datetime):
-    idf = {}
-    tweets = []
-    nr_topics = int(config['nmf']['nr_topics'])
-
-    topics_list = []
-
-    # get tweets from db
-    client = MongoClient(config['database']['host'], int(config['database']['port']))
-    db = client[config['database']['db']]
-    tweets_collection = db[config['tweets']['collection_name']]
-    topic_collection = db[config['nmf']['topic_collection_name']]
-
-    result = tweets_collection.find({'date_time':  {'$gte':  start_datetime, '$lt': stop_datetime}})
-    for tweet in result:
-        tweets += [Tweet.create_tweet(tweet)]
-
-    # find word document frequency
-    for tweet in tweets:
-        for word in tweet.words_map:
-            if word not in idf:
-                idf[word] = 0
-            idf[word] += 1
-
-    words = list(idf.keys())
-    word_indexes = {}
-    index = 0
-    for word in idf.keys():
-        word_indexes[word] = index
-        index += 1
-    index = 0
-    row = []
-    col = []
-    data = []
-
-    # prepare the date for nmf model
-    for tweet in tweets:
-        for word in tweet.words_map:
-            row += [index]
-            col += [word_indexes[word]]
-            data += [(tweet.words_map[word]/tweet.words_count) * math.log(tweets.__len__()/idf[word])]
-        index += 1
-    row = np.array(row)
-    col = np.array(col)
-    data = np.array(data)
-
-    # apply nmf
-    matrix = csr_matrix((data, (row, col)), shape=(tweets.__len__(), words.__len__()))
-    nmf_model = NMF(n_components=nr_topics, init='nndsvd', random_state=0)
-    relevant_tweets = nmf_model.fit_transform(matrix).tolist()
-    topics = nmf_model.components_
-
-    # extract topics and relevant tweets
-    index = 0
-    for topic in topics:
-        topic_object = Topic(start_datetime=start_datetime, stop_datetime=stop_datetime)
-        values = topic.tolist()
-        relevant_words = {}
-        for i in range(0, values.__len__()):
-            relevant_words[words[i]] = values[i]
-        relevant_words = sorted(relevant_words.items(), key=lambda x: x[1], reverse=True)
-
-        # add relevant words
-        topic_object.relevant_words = relevant_words[0:9]
-        relevant_tweets_per_topic = [(relevant_tweets[i][index], tweets[i]) for i in range(0, relevant_tweets.__len__())]
-        relevant_tweets_per_topic = sorted(relevant_tweets_per_topic, key=lambda x: x[0], reverse=True)
-
-        # add relevant tweet ids
-        topic_object.relevant_tweets = list(
-            map(lambda x: (x[0], x[1]._id), filter(lambda x: x[0] > 0.1, relevant_tweets_per_topic)))
-        topics_list += [topic_object.__dict__]
-        index += 1
-
-    topic_collection.insert(topics_list)
-
-    print("Processed topics in time interval", start_datetime, "-", stop_datetime, "consisting of", tweets.__len__(),
-          "tweets")
-
 
 if __name__ == '__main__':
-
     config = configparser.ConfigParser()
     config.read('./config/config.ini')
 
     if config['tweets']['load_tweets'].lower() == "true":
         load_tweets()
 
+    if config['topics']['create_topics'].lower() == "true":
+        create_and_store_topics()
+
     client = MongoClient(config['database']['host'], int(config['database']['port']))
     db = client[config['database']['db']]
-    date_hour_collection = db[config['tweets']['date_hour_collection_name']]
-    concurrent_tasks = int(config['default']['concurrent_tasks'])
-    topic_collection = db[config['nmf']['topic_collection_name']]
-    topic_collection.drop()
-    # add index on date-time
-    topic_collection.create_index([("date_time", pymongo.ASCENDING)])
-    executor = ProcessPoolExecutor(max_workers=concurrent_tasks)
+    topic_collection = db[config['topics']['topic_collection_name']]
 
-    date_hour_dict = date_hour_collection.find_one()
-    dates = []
-    futures = []
-    task_number = 0
+    topics_cursor = topic_collection.find()
+    topics = []
+    for topic in topics_cursor:
+        topics += [Topic.create_topic(topic)]
 
-    # date parsing
-    for key in date_hour_dict:
-        if key != "_id":
-            dates += [key]
-    dates.sort()
-    date_hour_dict[dates[0]].sort()
-    date_hour_dict[dates[dates.__len__() - 1]].sort(reverse=True)
+    topic_clusters = []
 
-    current_datetime_parts = re.split('-', dates[0])
-    limit_datetime_parts = re.split('-', dates[dates.__len__() - 1])
-
-    start_datetime = datetime.datetime(year=int(current_datetime_parts[0]), month=int(current_datetime_parts[1]),
-                                       day=int(current_datetime_parts[2]),
-                                       hour=int(date_hour_dict[dates[0]][0]))
-    stop_datetime = start_datetime + datetime.timedelta(hours=1)
-    limit_datetime = datetime.datetime(year=int(limit_datetime_parts[0]), month=int(limit_datetime_parts[1]),
-                                       day=int(limit_datetime_parts[2]),
-                                       hour=int(date_hour_dict[dates[dates.__len__() - 1]][0]))
-
-    # start tf-idf and nmf
-    start = time.time()
-    while start_datetime <= limit_datetime:
-        futures += [executor.submit(compute_nmf, config, start_datetime, stop_datetime)]
-
-        start_datetime += datetime.timedelta(minutes=10)
-        stop_datetime += datetime.timedelta(minutes=10)
-        task_number += 1
-        if task_number % concurrent_tasks == 0:
-            for future in futures:
-                future.result()
-            futures = []
-
-    for future in futures:
-        future.result()
-    end = time.time()
-
-    print("Processing nmf took: ", end - start)
-
-    for future in futures:
-        future.result()
-    end = time.time()
+    for topic in topics:
+        result = 0
+        assign_cluster = None
+        for cluster in topic_clusters:
+            current_result = cluster.cosine_similarity(topic)
+            if current_result > result and current_result > 0.7:
+                result = current_result
+                assign_cluster = cluster
+        if assign_cluster is None:
+            topic_clusters += [topic]
+        else:
+            assign_cluster.merge_with(topic)
+    for topic in topic_clusters:
+        print(topic.relevant_words)
--- topics.py
+++ topics.py
@@ -1,15 +1,59 @@
+from math import sqrt
+
+
 class Topic:
     def __init__(self, start_datetime="", stop_datetime="", relevant_words=[], relevant_tweets=[]):
         self.start_datetime = start_datetime
         self.stop_datetime = stop_datetime
         self.relevant_words = relevant_words
         self.relevant_tweets = relevant_tweets
+        self.time_periods = [(start_datetime, stop_datetime)]
+        self.merge_count = 0
 
     @staticmethod
     def create_topic(entries):
         topic = Topic()
         topic.__dict__.update(entries)
         return topic
+
+    def cosine_similarity(self, other_topic):
+        scalar_product = 0
+        norm1 = 0
+        norm2 = 0
+        word_dict_1 = {}
+        word_dict_2 = {}
+        for word_tuple in self.relevant_words:
+            word_dict_1[word_tuple[0]] = word_tuple[1]
+            norm1 += word_tuple[1] * word_tuple[1]
+
+        for word_tuple in other_topic.relevant_words:
+            word_dict_2[word_tuple[0]] = word_tuple[1]
+            norm2 += word_tuple[1] * word_tuple[1]
+
+        for word in word_dict_1:
+            if word in word_dict_2:
+                scalar_product += word_dict_1[word] * word_dict_2[word]
+
+        return scalar_product/(sqrt(norm1) * sqrt(norm2))
+
+    def merge_with(self, other_topic):
+        self.merge_count += 1
+        self.time_periods += [(other_topic.start_datetime, other_topic.stop_datetime)]
+        relevant_words = self.relevant_words + other_topic.relevant_words
+        relevant_words_dict = {}
+        for word_tuple in relevant_words:
+            if word_tuple[0] in relevant_words_dict:
+                relevant_words_dict[word_tuple[0]] = (relevant_words_dict[word_tuple[0]] *
+                                                      self.merge_count + word_tuple[1]) / (self.merge_count + 1)
+            else:
+                relevant_words_dict[word_tuple[0]] = word_tuple[1]
+        self.relevant_words = sorted(relevant_words_dict.items(), key=lambda x: x[1], reverse=True)[0:9]
+        relevant_tweets_map = {}
+        for tweet_tuple in self.relevant_tweets + other_topic.relevant_tweets:
+            if tweet_tuple[1] not in relevant_tweets_map:
+                relevant_tweets_map[tweet_tuple[1]] = tweet_tuple[0]
+        self.relevant_tweets = [(relevant_tweets_map[tweet], tweet) for tweet in relevant_tweets_map ]
+
 
 
 
